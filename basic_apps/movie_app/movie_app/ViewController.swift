ae401fc26f570aac9f3c2ad31faf23c34faadb60
LucasGeek <lucas.albuquerque.gk@gmail.com>
2020-07-01 19:19:12 -0300

    First app movie

---
+++ basic_apps/movie_app/movie_app/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  movie_app
+//
+//  Created by Lucas Albuquerque on 01/07/20.
+//  Copyright © 2020 Lucas Geek. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
